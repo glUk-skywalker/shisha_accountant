8118c289578c44e909bcff9dd63e4ea370e1b1bc
gluk <gluk.main+github@gmail.com>
2018-05-11 20:31:47 +0300

    Message class refactored and improved
    self.for(user) method(new): only accepts user (more intuitive)
    message text now can be set via .text = 'foo bar'
    message keyboard now can be set via .keys = <keys structure>
    message parameters are now generated on the fly

--- bot/lib/message.rb
+++ bot/lib/message.rb
@@ -1,26 +1,42 @@
 class Message
-  def initialize(user, text = '', kbd = nil)
-    @params = {
-      chat_id: user.id,
-      text: text,
-      reply_markup: kbd ? markup(kbd) : nil
-    }
+  attr_accessor :text
+
+  def initialize(user, text = '', keys_set = nil)
+    @user = user
+    @text = text
+    @keys_makup = keys_set ? markup(keys_set) : nil
     @token = Rails.application.secrets.bot_token
+  end
+
+  def self.for(user)
+    new user
+  end
+
+  def keys=(keys_set)
+    @keys_makup = keys_set ? markup(keys_set) : nil
   end
 
   def send!
     with_bot do |bot|
-      bot.api.send_message(@params)
+      bot.api.send_message(params)
     end
   end
 
   def update!(message_id)
     with_bot do |bot|
-      bot.api.edit_message_text(@params.merge(message_id: message_id))
+      bot.api.edit_message_text(params.merge(message_id: message_id))
     end
   end
 
   private
+
+  def params
+    {
+      chat_id: @user.id,
+      text: @text,
+      reply_markup: @keys_makup
+    }
+  end
 
   def markup(kbd)
     Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kbd)
