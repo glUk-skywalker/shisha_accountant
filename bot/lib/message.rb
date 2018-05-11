47fe56e347f57cac2ae004249f9dfb27fbeb924d
gluk <gluk.main+github@gmail.com>
2018-05-11 20:19:37 +0300

    Proper send! and update! code for Message class

--- bot/lib/message.rb
+++ bot/lib/message.rb
@@ -5,19 +5,30 @@ class Message
       text: text,
       reply_markup: kbd ? markup(kbd) : nil
     }
+    @token = Rails.application.secrets.bot_token
   end
 
   def send!
-    bot.api.send_message(@params)
+    with_bot do |bot|
+      bot.api.send_message(@params)
+    end
   end
 
   def update!(message_id)
-    bot.api.edit_message_text(@params.merge(message_id: message_id))
+    with_bot do |bot|
+      bot.api.edit_message_text(@params.merge(message_id: message_id))
+    end
   end
 
   private
 
   def markup(kbd)
     Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kbd)
+  end
+
+  def with_bot
+    Telegram::Bot::Client.run(@token) do |bot|
+      yield(bot)
+    end
   end
 end
