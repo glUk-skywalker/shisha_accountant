75d18b0d2461ddf83f06d5f5067992c1572ebbb5
gluk <gluk.main+github@gmail.com>
2018-05-16 00:21:10 +0300

    Small refactoring for Bot msg method
    Using User.draw.participants instead of manual text generating

--- bot/lib/msg.rb
+++ bot/lib/msg.rb
@@ -3,13 +3,7 @@ def msg(user)
 
   msg_lines = []
   Shisha.current.each do |s|
-    shisha_users = s.users
-    if shisha_users.include?(user)
-      other_users = shisha_users - [user]
-      msg_lines << (['You'] + other_users.map(&:first_name)).to_sentence
-    else
-      msg_lines << shisha_users.map(&:first_name).to_sentence
-    end
+    msg_lines << s.draw.participants(you: user)
   end
 
   msg = if msg_lines.any?
