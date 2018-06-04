5f0dba49fca695bb82a1a37d5b7185723b298977
gluk <gluk.main+github@gmail.com>
2018-06-04 08:47:56 +0300

    Finish smoking button is renamed to Pay and stop to be more clear

--- bot/lib/buttons.rb
+++ bot/lib/buttons.rb
@@ -1,9 +1,9 @@
 class Buttons
   STATIC_TYPES = {
-    leave:   { text: 'Leave',          callback_data: 'leave' },
-    finish:  { text: 'Finish smoking', callback_data: 'stop' },
-    create:  { text: 'Set up new!',    callback_data: 'create' },
-    refresh: { text: '↻',              callback_data: '↻' }
+    leave:   { text: 'Leave',        callback_data: 'leave' },
+    finish:  { text: 'Pay and stop', callback_data: 'stop' },
+    create:  { text: 'Set up new!',  callback_data: 'create' },
+    refresh: { text: '↻',            callback_data: '↻' }
   }.freeze
 
   def self.build(params)
