63e5d081d21a4f0d4aab4b07a1a9e7253c4d3bed
gluk <gluk.main+github@gmail.com>
2018-07-03 19:37:17 +0300

    The logic for processiong not allowed users is adjusted.

--- bot/run.rb
+++ bot/run.rb
@@ -11,36 +11,49 @@ token = Rails.application.secrets.bot_token
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
-    user = User.where(id: message.from.id).first
+    user = User.unscoped.where(id: message.from.id).first
     user ||= User.create(message.from.to_h)
 
     case message
     when Telegram::Bot::Types::CallbackQuery
-      user_message = Message.new(user, '↻ Working...')
-      user_message.update!(message.message.message_id)
+      user.message.text = '↻ Working..'
+      user.message.update!(message.message.message_id)
 
-      if user.allowed
-        case message.data
-        when 'create'
-          user.action.create_shisha
-        when /join:\d+/
-          shisha_id = message.data.split(':').last
-          s = Shisha.where(id: shisha_id).first
-          user.action.join_shisha(s)
-        when 'leave'
-          user.action.leave_shisha
-        when 'stop'
-          user.action.stop_shisha
-        when /accept_user:\d+/
-          user_id = message.data.split(':').last
-          User.find(user_id).update_attributes(allowed: true)
-        end
+      unless user.allowed?
+        user.message.text = USER_NOT_ALLOWED_MESSAGE_TEXT
+        user.message.update!(message.message.message_id)
+        next
       end
 
-      user_message.text = msg(user)
-      user_message.keys = kb(user)
-      user_message.update! message.message.message_id
+      case message.data
+      when 'create'
+        user.action.create_shisha
+      when /join:\d+/
+        shisha_id = message.data.split(':').last
+        s = Shisha.where(id: shisha_id).first
+        user.action.join_shisha(s)
+      when 'leave'
+        user.action.leave_shisha
+      when 'stop'
+        user.action.stop_shisha
+      when /accept_user:\d+/
+        user_id = message.data.split(':').last
+        accepted_user = User.find(user_id)
+        accepted_user.update_attributes(allowed: true)
+        accepted_user.message.text = 'You have been accept for using this bot!'
+        accepted_user.message.send!
+      end
+
+      user.message.text = msg(user)
+      user.message.keys = kb(user)
+      user.message.update! message.message.message_id
     when Telegram::Bot::Types::Message
+      unless user.allowed?
+        user.message.text = USER_NOT_ALLOWED_MESSAGE_TEXT
+        user.message.send!
+        next
+      end
+
       case message.text
       when '/start'
         bot.api.send_message(chat_id: message.from.id, text: "Hello, #{message.from.first_name}")
@@ -49,11 +62,7 @@ Telegram::Bot::Client.run(token) do |bot|
         user.message.keys = kb(user)
         user.message.send!
       when '/login_link'
-        if user.allowed?
-          user.message.login_link.send!
-        else
-          bot.api.send_message(chat_id: message.from.id, text: 'You are not allowed user')
-        end
+        user.message.login_link.send!
       when '/history'
         user.message.history.send!
       when '/notifications_on'
---
+++ config/initializers/constants.rb
@@ -0,0 +1,5 @@
+USER_NOT_ALLOWED_MESSAGE_TEXT = <<~HEREDOC.freeze
+  Sorry... You are not allowed to use this bot.
+  The message you want to join was sent to the admins.
+  You will be notified when accepted.
+HEREDOC
