43dfd08fa459f8bb640e5e3dfd6ebd778e0cd1c2
gluk <gluk.main+github@gmail.com>
2018-08-28 21:34:52 +0300

    User account change now can be passed to the global events
    Event view templates
    - new (amended): includes the 'global' checkbox
    
    EventsController
    - create action (amended): creates  a glob event if checkbox
    
    GloblEvent view templates
    - index (amended): a bit reorganized + money highlight

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,6 +7,11 @@ class EventsController < AuthenticatedUserController
   def create
     user = User.find(params[:user_id])
     user.change_money(event_params[:change].to_f, event_params[:comment])
+    if params[:global]
+      e = GlobalEvent.new(event_params)
+      e.comment = '[' + user.first_name + '] ' + e.comment
+      e.save
+    end
     redirect_to users_path
   end
 
