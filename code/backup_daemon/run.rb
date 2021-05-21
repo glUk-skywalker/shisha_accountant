7ff32a305801203b5235a5baa5f4853467136f72
gluk <gluk.main+github@gmail.com>
2021-05-21 13:26:16 +0300

    backing up: only considering FINISHED shishas

--- code/backup_daemon/run.rb
+++ code/backup_daemon/run.rb
@@ -15,7 +15,7 @@ def backup_now?
   last_time = Time.parse File.read(TIME_FILEPATH)
   return false if (Time.now - last_time) < 60 * 60 * 24
 
-  Shisha.last.updated_at > last_time
+  Shisha.finished.last.updated_at > last_time
 rescue
   true
 end
