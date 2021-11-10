549fef05d10fbd5449d6fd98b7dcf422df52cff6
gluk <gluk.main+github@gmail.com>
2021-11-10 19:37:07 +0300

    serivce daemon: now reminding debtors (+adjustments)

--- code/service_daemon/run.rb
+++ code/service_daemon/run.rb
@@ -1,18 +1,34 @@
-puts 'Loading Rails env...'
 require File.expand_path('../../config/environment', __FILE__)
-puts 'Loaded!'
 
-puts 'Connecting to the db...'
+logger = Logger.new("#{Rails.root}/log/service_daemon.log")
+logger.info 'Rails environment has been successfully loaded!'
+
 config = Rails.application.config.database_configuration[Rails.env]
 ActiveRecord::Base.establish_connection(config)
-puts 'Connected!'
+logger.info 'DB connection has been successfully established!'
+
+$debtors_reminded = false
+
+def time_to_remind_debtors?
+  time = Time.now
+
+  $debtors_reminded = false if time.hour >= 20
+
+  return false if $debtors_reminded
+
+  return false if time.saturday? || time.sunday?
+
+  time.hour == 19
+end
 
 loop do
+  sleep 60
+
   Shisha.stop_old!
 
-  # TODO
-  # DBBackuper.backup!
-  # User.remind_debtors!
-
-  sleep 60
+  if time_to_remind_debtors?
+    logger.info 'reminding debtors..'
+    User.remind_debtors!
+    $debtors_reminded = true
+  end
 end
